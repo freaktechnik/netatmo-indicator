d47e0f91af8bf4a14d3c18156c26ad9e3dac97fa
Martin Giger <martin@humanoids.be>
2022-10-26 19:21:26 +0200

    ci: try out dependabot for actions

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const clientSecret = '', // eslint-disable-line no-unused-vars
-    clientToken = ''; // eslint-disable-line no-unused-vars
+const clientSecret = 'LSQoqV3j7xEezXPHXG4uVCs73TwbGeonrNHMFCuMew', // eslint-disable-line no-unused-vars
+    clientToken = '5b55e6cbac34a57d198b490c'; // eslint-disable-line no-unused-vars
